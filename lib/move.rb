bb430d3d4cb27f52b748c5e3e2b398f20cdbf244
Abdul <awsayed@alaska.edu>
2019-05-30 00:07:00 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -1,3 +1,6 @@
+
+# board is an array size 9 of "X", "O", or ""
+
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
@@ -7,3 +10,12 @@ def display_board(board)
 end
 
 # code your input_to_index and move method here!
+
+def input_to_index(input)
+  # Given input "1" - "9", convert to num 0 - 8
+  input = input.to_i - 1 
+end
+
+def move
+  
+end
